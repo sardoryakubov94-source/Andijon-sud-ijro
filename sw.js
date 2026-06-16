22bd5c03fab41a7ba04cffd2f528c0729ccef52e
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-06-16 09:32:47 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sud-ijro-v9';
+const CACHE='sud-ijro-v10';
 const SELF_URL = self.location.pathname.replace(/sw\.js$/, 'Andijon_Sud_Ijro-19-6.html');
 
 self.addEventListener('install', e=>{
